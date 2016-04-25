2769d5e52d112546f71485e951394abc8ec382cf
AhmadzaiNajeebUllah <najeebahmadzai14u@gmail.com>
2016-04-25 17:40:44 +0500

    you have not change yet

---
+++ NajeebSCM/src/SCM/Scm.java
@@ -0,0 +1,11 @@
+package SCM;
+
+public class Scm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+System.out.println("How r u?");
+	}
+
+}
